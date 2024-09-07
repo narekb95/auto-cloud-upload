102e437e64256d64b04d702b32752696fe890909
bojikian <bojikian@hu-berlin.de>
2024-09-07 23:41:20 +0200

    added source code

---
+++ upload_files.py
@@ -0,0 +1,45 @@
+from shutil import copyfile
+import json
+import time
+import os
+
+def file_exists_and_changed(last_check, file):
+    if not os.path.isfile(file):
+        return False
+    time = os.path.getmtime(file)
+    # print(f'File {file} time {time}, last check {last_check}')
+    return time > last_check
+
+def read_config(file):
+    f = open(file, 'r')
+    data = f.read()
+    config = json.loads(data)
+    f.close()
+    return config
+
+def update_config(file, config):
+    out = json.dumps(config)
+    f = open(file, 'w')
+    f.write(out)
+    f.close()
+
+config = read_config('config.json')
+last_check = config['last-check'] if 'last-check' in config else 0
+config['last-check'] = int(time.time())
+update_config('config.json', config)
+
+target_dir = config['target']
+files = config['registry']
+
+for file in files:
+    path = file['path']
+    name = file['name']
+    target_file = target_dir + name
+    if file_exists_and_changed(last_check, path):
+        copyfile(path, target_file)
+        print(f'File \"{name}\" was updated.')
+    else:
+        print(f'File \"{name}\" already uptodate.')
+
+
+print()

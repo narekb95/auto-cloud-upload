37fbbc42684682811d17d942ed89d876ac4614b9
narekb95 <narekb95@gmail.com>
2024-09-17 13:29:12 +0200

    update

--- helpers.py
+++ helpers.py
@@ -1,9 +1,7 @@
 import json
 import datetime
 def read_arg(target : str, argv):
-    # Read the parameters of syntax --arg=value
-    # and return the value
-    arg =  next((a for a in argv if a.startswith('--') and a[2:2 + len(target)] == target), None)
+    arg =  next((a for a in argv if a.startswith(f'--{target}=')), None)
     return arg.split('=')[1] if arg != None else None
 
 def log(s):
@@ -14,14 +12,12 @@ def timestamp_to_date(ts):
 
 
 def read_config(file):
-    f = open(file, 'r')
-    data = f.read()
-    config = json.loads(data)
-    f.close()
+    with open(file, 'r') as f: 
+        data = f.read()
+        config = json.loads(data)
     return config
 
 def update_config(file, config):
     out = json.dumps(config)
-    f = open(file, 'w')
-    f.write(out)
-    f.close()
+    with open(file, 'w') as f:
+        f.write(out)
--- upload_files.py
+++ upload_files.py
@@ -4,12 +4,8 @@ import json
 import time
 import os
 
-<<<<<<< HEAD
 from helpers import read_arg, log, timestamp_to_date, update_config, read_config
 
-=======
-from helpers import read_arg, log, timestamp_to_date
->>>>>>> 5109eddf0bbe66b02c410a3ac70abd8a3e542b5d
 def file_exists_and_changed(last_check, file):
     if not os.path.isfile(file):
         return False
@@ -18,46 +14,25 @@ def file_exists_and_changed(last_check, file):
     return time > last_check
 
 
-try:
+config_file = read_arg('config-file', argv)
+if config_file is None:
+    raise Exception('Config file not provided.')
 
-    config_file = read_arg('config-file', argv)
-    if config_file is None:
-        raise Exception('Config file not provided.')
-    
-    config = read_config(config_file)
-    target_dir = config['target']
-    files = config['registry']
-    curr_timestamp = int(time.time())
-
-<<<<<<< HEAD
-last_check = config['last-check'] if 'last-check' in config else 0
-config['last-check'] = int(time.time())
-update_config('config.json', config)
-
+config = read_config(config_file)
 target_dir = config['target']
 files = config['registry']
+curr_timestamp = int(time.time())
 
 for file in files:
     path = file['path']
     name = file['name']
+    last_update = file['last-update'] if 'last-update' in file else 0
     target_file = target_dir + name
-    if file_exists_and_changed(last_check, path):
+    if file_exists_and_changed(last_update, path):
         copyfile(path, target_file)
-        log(f'{timestamp_to_date(time.time())} updated file \"{name}\".')
-=======
-    for file in files:
-        path = file['path']
-        name = file['name']
-        last_update = file['last-update'] if 'last-update' in file else 0
-        target_file = target_dir + name
-        if file_exists_and_changed(last_update, path):
-            copyfile(path, target_file)
-            file['last-update'] = curr_timestamp
-            print(f'File {name} updated.')
-        else: 
-            print(f'File {name} uptodate.')
-    
-    update_config(config_file, config)
-except Exception as e:
-    print(f'Error: {e}')
->>>>>>> 5109eddf0bbe66b02c410a3ac70abd8a3e542b5d
+        file['last-update'] = curr_timestamp
+        print(f'File {name} updated.')
+    else: 
+        print(f'File {name} uptodate.')
+
+update_config(config_file, config)

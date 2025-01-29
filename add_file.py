23c157c8f4347eaf34b9ff9dad96be20d33e1e1d
narekb95 <narekb95@gmail.com>
2025-01-29 16:50:02 +0100

    separated add-file function from file

--- add_file.py
+++ add_file.py
@@ -2,24 +2,30 @@ from sys import argv
 
 from config import Config
 
+
+def add_file(path, name, config):
+    extension = path.split('.')[-1]
+    name = (f'{name}.{extension}').lower()
+    file = next((file for file in config.files if file['name'].lower() == name), None)
+    if file is not None:
+        raise ValueError(f'File {name} already exists in auto-upload.')
+    config.files.append({'path': path, 'name': name})
+    config.update_config()
+
+
 path = argv[1]
 config = Config()
 
-name = None
 valid_name = False
 while not valid_name:
     name = input('Target file name: ')
-    file = next((file for file in config.files\
-                  if file['name'].lower() == name.lower()), None)
-    if file is not None:
-        print(f'File {name} already exists in auto-upload.')
-    else:
-        valid_name = True
-
-config.files.append({'path': path, 'name': name})
-try:
-    config.update_config()
-    print(f'File {name} added to auto-upload.')
-except Exception as e:
-    print(f'Failed to add file {name}. Error: {e}')
+    valid_name = True
+    try:
+        add_file(path, name, config)
+        print(f'File {name} added to auto-upload.')
+    except ValueError as e:
+        print(f'Value error: {e}')
+        valid_name = False
+    except Exception as e:
+        print(f'Failed to add file {name}. Error: {e}')
 input('Press Enter to exit...')

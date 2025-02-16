845f16f69ac302ee00c0a3e7a4dc06631d3d21c5
bojikian <bojikian@hu-berlin.de>
2025-02-16 23:33:14 +0100

    "added select target folder button"

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import config as cnf
+import tkinter.filedialog
 
 class Setting:
     def __init__(self, label, attribute, value):
@@ -36,22 +37,29 @@ def create_settings_window(dialog, config):
     settings_frame = tk.Frame(dialog)
     settings_frame.pack(fill=tk.X, expand=True, anchor="n", padx=30, pady=10)
     settings_frame.columnconfigure(0, weight=0)
-    settings_frame.columnconfigure(1, weight=1)
-    settings_frame.columnconfigure(2, weight=0)
+    settings_frame.columnconfigure(1, weight=0)
+    settings_frame.columnconfigure(2, weight=1)
     settings = get_current_settings(config)
     for i, setting in enumerate(settings):
         tk.Label(settings_frame, text=setting.label, font=('Ariel', 10))\
             .grid(row=i, column=0, padx=10, pady=10, sticky="w")
         setting_entry = tk.Entry(settings_frame, textvariable=setting.var,\
                 width = 30, font=('Ariel', 10))
-        setting_entry.grid(row=i, column=1, padx=10, pady=10, sticky="ew")
+        setting_entry.grid(row=i, column=2, padx=10, pady=10, sticky="ew")
         if i == 0:
             setting_entry.focus_set()
             setting_entry.select_range(0, tk.END)
     
-    target_button = tk.Button(settings_frame, text="Browse", command=lambda setting=settings[0]:\
-                                setting.var.set(tk.filedialog.askdirectory()))
-    target_button.grid(row=0, column=2, padx=10, pady=10, sticky="w")
+    def update_directory():
+        folder =tk.filedialog.askdirectory()
+        if folder:
+            settings[0].var.set(folder)
+
+    folder_icon = tk.PhotoImage(file="media/buttons/folder.png")
+    target_folder_button = tk.Button(settings_frame, image=folder_icon, \
+        command = update_directory)
+    target_folder_button.image = folder_icon  # Keep a reference to avoid garbage collection
+    target_folder_button.grid(row=0, column=1, padx=0, pady=10, sticky="w")
 
     submit_frame = tk.Frame(dialog)
     submit_frame.pack(expand=True)

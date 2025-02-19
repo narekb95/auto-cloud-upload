16690b752e62366c91fb92f09c9fe4b8d8fadd1d
bojikian <bojikian@hu-berlin.de>
2025-02-19 02:40:53 +0100

    added more complex UX/UI

--- diagnostic.py
+++ diagnostic.py
@@ -40,23 +40,17 @@ def remove_files(files):
     with config.lock:
         data_man.remove_files(files)
 
-def delete_selected():
-    files = get_selected_files()
-    remove_files(files)
+def delete_selected(tree):
+    files = get_selected_files(tree)
     for file in files:
         delete_file(path.join(config.target_dir, file))
 
-def handle_right_click(event):
-    current_item = synced_tree.identify_row(event.y)
-    if current_item and current_item not in synced_tree.selection():
-        synced_tree.selection_set(current_item)
-    menu = tk.Menu(root, tearoff=0)
-    menu.add_command(label="Add Files", command=on_add_file)
-    menu.add_command(label="Open", command=open_selected, state=tk.DISABLED if len(synced_tree.selection()) == 0 else tk.NORMAL)
-    menu.add_command(label="Remove", command=remove_selected, state=tk.DISABLED if len(synced_tree.selection()) == 0 else tk.NORMAL)
-    menu.add_command(label="Delete", command=delete_selected, state=tk.DISABLED if len(synced_tree.selection()) == 0 else tk.NORMAL)
-    menu.post(event.x_root, event.y_root)
-
+def remove_and_delete_selected(tree):
+    files = get_selected_files(tree)
+    # This order is important, otherwise get into race condition
+    for file in files:
+        delete_file(path.join(config.target_dir, file))
+    remove_files(files)
 
 def refresh_table():
     global last_check
@@ -66,6 +60,11 @@ def refresh_table():
     target_dir_var.set(config.target_dir)
     refresh_unsynced_files()
 
+def clear_other_selection(event, other_tree):
+    selected = event.widget.selection()
+    if not selected:
+        return
+    other_tree.selection_remove(other_tree.selection())
 
 
 def on_open_file(tree, event):
@@ -81,26 +80,16 @@ def create_table():
     tree.pack(fill=tk.BOTH, expand=True)
 
 
-    tree.bind("<Double-Button-1>", lambda e: on_open_file(synced_tree, e))
-    # if empty area is clicked, deselect all
-    def empty_click_handler(event):
-        item = tree.identify_row(event.y)
-        if not item:
-            tree.selection_remove(tree.selection())
-    tree.bind("<Button-1>", empty_click_handler)
-    tree.bind("<Button-3>", handle_right_click)
+def get_selected_files(tree):
+    return [tree.item(item, "values")[0] for item in tree.selection()]
 
-
-def get_selected_files():
-    return [synced_tree.item(item, "values")[0] for item in synced_tree.selection()]
-
-def open_selected():
-    selected_files = get_selected_files()
+def open_selected(tree):
+    selected_files = get_selected_files(tree)
     open_files(selected_files)
 
 def remove_selected():
     selected_items = synced_tree.selection()
-    selected_files = get_selected_files()
+    selected_files = get_selected_files(synced_tree)
 
     remove_files(selected_files)
     for item in reversed(selected_items):
@@ -116,7 +105,6 @@ def delete_unsynced_files():
     print('Unsynced files:', unsynced_files)
     for file in unsynced_files:
         delete_file(path.join(dir, file))
-        print(f'Deleted file {file}')
     refresh_unsynced_files()
 
 def refresh_unsynced_files():
@@ -133,6 +121,12 @@ def on_add_file():
         print(e)
     refresh_table()
 
+def empty_click_handler(event, tree):
+    item = tree.identify_row(event.y)
+    if not item:
+        synced_tree.selection_remove(synced_tree.selection())
+        unsynced_tree.selection_remove(unsynced_tree.selection())
+
 def create_synced_files_frame(window):
     synced_files_frame = tk.Frame(window)
     window.add(synced_files_frame)
@@ -145,7 +139,41 @@ def create_synced_files_frame(window):
     synced_tree.heading("Timestamp", text="Timestamp")
     synced_tree.column("Name", anchor="w")
     synced_tree.column("Timestamp", anchor="w")
+
+    
+    synced_tree.bind("<Double-Button-1>", lambda e: on_open_file(synced_tree, e))
+    synced_tree.bind("<Button-1>", lambda e: empty_click_handler(e, synced_tree))
+    synced_tree.bind("<Button-3>", handle_synced_right_click)
+    synced_tree.bind("<<TreeviewSelect>>", lambda e: clear_other_selection(e, unsynced_tree))
+
     refresh_table()
+
+def handle_synced_right_click(event):
+    unsynced_tree.selection_remove(unsynced_tree.selection())
+    current_item = synced_tree.identify_row(event.y)
+    if current_item and current_item not in synced_tree.selection():
+        synced_tree.selection_set(current_item)
+    menu = tk.Menu(root, tearoff=0)
+    menu.add_command(label="Add Files", command=on_add_file)
+    menu.add_command(label="Open file(s)", command=lambda: open_selected(synced_tree), state=tk.DISABLED if len(synced_tree.selection()) == 0 else tk.NORMAL)
+    menu.add_command(label="Remove file(s)", command=remove_selected, state=tk.DISABLED if len(synced_tree.selection()) == 0 else tk.NORMAL)
+    menu.add_command(label="Delete file(s)", command=lambda: remove_and_delete_selected(synced_tree), state=tk.DISABLED if len(synced_tree.selection()) == 0 else tk.NORMAL)
+    menu.post(event.x_root, event.y_root)
+
+
+def handle_unsynced_right_click(event):
+    synced_tree.selection_remove(synced_tree.selection())
+
+    current_item = unsynced_tree.identify_row(event.y)
+    if current_item and current_item not in unsynced_tree.selection():
+        unsynced_tree.selection_set(current_item)
+    menu = tk.Menu(root, tearoff=0)
+    menu.add_command(label="Open", command=lambda: open_selected(unsynced_tree), state=tk.DISABLED if len(unsynced_tree.selection()) == 0 else tk.NORMAL)
+    menu.add_command(label="Delete", command=lambda: delete_selected(unsynced_tree), state=tk.DISABLED if len(unsynced_tree.selection()) == 0 else tk.NORMAL)
+    menu.add_command(label="Delete All", command=lambda: delete_unsynced_files(), state=tk.DISABLED if unsynced_tree.get_children() == () else tk.NORMAL)
+    menu.post(event.x_root, event.y_root)
+
+
 
 def create_unsynced_files_frame(window):
     unsynced_files_frame = tk.Frame(window)
@@ -156,8 +184,10 @@ def create_unsynced_files_frame(window):
     unsynced_tree = ttk.Treeview(unsynced_files_frame, columns=("Name",), show="")
     unsynced_tree.heading("Name", text="Name")
     unsynced_tree.column("Name", anchor="w")
-    # unsynced_tree.bind("<Button-1>", lambda e: "break")
+    unsynced_tree.bind("<Button-3>", handle_unsynced_right_click)
+    unsynced_tree.bind("<<TreeviewSelect>>", lambda e: clear_other_selection(e, synced_tree))
     unsynced_tree.bind("<Double-Button-1>", lambda e: on_open_file(unsynced_tree, e))
+    unsynced_tree.bind("<Button-1>", lambda e: empty_click_handler(e, unsynced_tree))
 
     unsynced_tree.pack(fill=tk.BOTH, expand=True)
     refresh_unsynced_files()
@@ -230,13 +260,6 @@ def main():
         time.sleep(.2)
         refresh_unsynced_files()
 
-
-    file_observer = FileChangeHandler([get_data_file()], on_data_update)
-    file_observer.start()
-
-    target_folder_observer = FolderDeletionHandler(config.target_dir, on_target_folder_update)
-    target_folder_observer.start()
-
     def config_update(_):
         old_target = config.target_dir
         config.read_config()
@@ -245,6 +268,12 @@ def main():
             refresh_unsynced_files()
             target_dir_var.set(config.target_dir)
             target_folder_observer.update_watched_dirs(config.target_dir)
+
+    file_observer = FileChangeHandler([get_data_file()], on_data_update)
+    file_observer.start()
+
+    target_folder_observer = FolderDeletionHandler(config.target_dir, on_target_folder_update)
+    target_folder_observer.start()
 
     config_observer = FileChangeHandler([get_config_file()], config_update)
     config_observer.start()
